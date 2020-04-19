c4d6c46f87ea7f3d5c45f208e29b08bb1d4ce6a9
Kanav Bhatnagar <kanavb2@illinois.edu>
2020-04-19 09:28:42 -0500

    Create needed functionality for the engine
    Add functions to manage the game engine, like stepping in a certain direction moving the player character.

---
+++ include/island/direction.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2020 Kanav Bhatnagar. All rights reserved.
+
+#ifndef ISLAND_DIRECTION_H_
+#define ISLAND_DIRECTION_H_
+
+namespace island {
+
+enum class Direction {
+  kUp;
+  kDown;
+  kLeft;
+  kRight;
+};
+
+}  // namespace island
+
+#endif // ISLAND_DIRECTION_H_
--- include/island/engine.h
+++ include/island/engine.h
@@ -3,7 +3,42 @@
 #ifndef ISLAND_ENGINE_H_
 #define ISLAND_ENGINE_H_
 
+#include "direction.h"
+
 namespace island {
+
+/**
+ * This is the game engine; the primary way to interact with the game.
+ */
+class Engine {
+ public:
+  /**
+   * Creates a new game for the island.
+   *
+   * @param width the width of the island map
+   * @param height the height of the island map
+   */
+  Engine(size_t width, size_t height);
+
+  /** Executes a time step, moves the player character. */
+  void Step();
+
+  /** Saves the game. */
+  void Save();
+
+  /** Changes the direction of the player character with each step. */
+  void SetDirection(const Direction& direction);
+
+ private:
+  /** The width of the island map. */
+  const size_t width_;
+
+  /** The height of the island map. */
+  const size_t height_;
+
+  /** The direction the player character moves in. */
+  const Direction direction_;
+};
 
 }  // namespace island
 
