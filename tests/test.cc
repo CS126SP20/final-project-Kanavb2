a4ffd79a1ccf0eae180fe700beeeb4118df27cc7
Kanav Bhatnagar <kanavb2@illinois.edu>
2020-04-21 21:04:47 -0500

    Test for location operator overloads.
    Test whether the location addition and modulo operations work as intended.

--- tests/test.cc
+++ tests/test.cc
@@ -2,13 +2,25 @@
 
 #define CATCH_CONFIG_MAIN
 
-#include <cinder/Rand.h>
 #include <island/engine.h>
+#include <island/location.h>
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("Random sanity test", "[random]") {
-  const float random = cinder::randFloat();
-  REQUIRE(0. <= random);
-  REQUIRE(random <= 1.);
+TEST_CASE("Location addition overload test", "[location]") {
+  island::Location first_location(10, 20);
+  island::Location second_location(5, 10);
+  island::Location location = first_location + second_location;
+
+  REQUIRE(location.GetRow() == 15);
+  REQUIRE(location.GetCol() == 30);
+}
+
+TEST_CASE("Location mod overload test", "[location]") {
+  island::Location first_location(10, 20);
+  island::Location second_location(4, 10);
+  island::Location location = first_location % second_location;
+
+  REQUIRE(location.GetRow() == 2);
+  REQUIRE(location.GetCol() == 0);
 }
